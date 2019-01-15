89e20384488e6632cccc5666440463871562f010
mixassio <mixail.bondarev@gmail.com>
2019-01-15 09:41:26 +0300

    gon from webpack

--- src/index.js
+++ src/index.js
@@ -1,10 +1,10 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
 import '../assets/application.css';
 import ReactDOM from 'react-dom';
+import gon from 'gon';
 import listChannels from './ListChannels';
 
 // import faker from 'faker';
-// import gon from 'gon';
 // import cookies from 'js-cookie';
 // import io from 'socket.io-client';
 
@@ -12,7 +12,7 @@ if (process.env.NODE_ENV !== 'production') {
   localStorage.debug = 'chat:*';
 }
 
-const { channels } = window.gon;
+const { channels } = gon;
 
 ReactDOM.render(
   listChannels(channels),
