d809ece9b8d586dbd7103e20e9d0488de6c6d2b1
mixassio <mixail.bondarev@gmail.com>
2019-02-18 08:22:21 +0300

    add ref

--- src/components/FormMessage.jsx
+++ src/components/FormMessage.jsx
@@ -12,6 +12,20 @@ const mapStateToProps = ({ currentChannelId, addMessage }) => ({
 @connect(mapStateToProps)
 @reduxForm({ form: 'newMessage' })
 class FormMessage extends React.Component {
+  constructor(props) {
+    super(props);
+    this.inputMessage = React.createRef();
+  }
+
+  componentDidMount() {
+    console.log('didMount', this.inputMessage.current);
+  }
+
+  componentDidUpdate() {
+    console.log('didUpdate', this.inputMessage.current);
+    this.inputMessage.getRenderedComponent().focus();
+  }
+
   submitMessage = user => async (value) => {
     const {
       reset,
@@ -36,7 +50,18 @@ class FormMessage extends React.Component {
       <UserConsumer>
         {user => (
           <form className="form-inline mt-3" onSubmit={handleSubmit(this.submitMessage(user))}>
-            <Field name="text" required component="input" type="text" disabled={submitting} className="w-75 border border-info" />
+            <Field
+              autoComplete="off"
+              ref={this.inputMessage}
+              withRef
+              autoFocus
+              name="text"
+              required
+              component="input"
+              type="text"
+              disabled={submitting}
+              className="w-75 border border-info"
+            />
             <button type="submit" className="ml-1 btn btn-primary btn-sm" disabled={submitting}>Send</button>
           </form>
         )}
